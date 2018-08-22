820a45bc60b712612e9adf8f7784cfe7b42bfcc9
lquijano <quijano1012@gmail.com>
2018-08-21 18:27:41 -0700

    adding apiRoutes and htmlRoutes.js files

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,35 @@
+var friends = require("../data/friends");
+
+module.exports = function(app){
+  app.get("/api/friends" function(request, response) {
+    response.json(friends);
+    });
+  app.post("/api/friends" function(request, response) {
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: Infinity
+    };
+  var userData = request.body;
+  var userScores = userData.scores;
+  var totalDifference;
+
+  // here we loop through all the friend possibilities in the database
+
+  for (var i = 0; i < friends.length; i++) {
+    var currentFriend = friends[i];
+    totalDifference = 0;
+    console.log(currentFriend.name);
+
+  for (var j = 0; j < currentFriend.scores.length; i++) {
+    var currentFriendScore = currentFriend.scores[j];
+    var currentUserScore = userScores[j];
+
+// we calculate the difference between the scores and sum them into the total difference
+    totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+// to be continued in our next session
+
+      }
+    };
+  });
+};
---
+++ app/server.js
@@ -0,0 +1,22 @@
+// Dependencies
+// series of npm packages that we will use to give our server useful functionality
+// BodyParser makes it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// invoke express
+// this tells node we are creating an express server
+var app = express();
+
+var PORT = 8080;
+
+// app.use(bodyParser.json()) basically tells the system that you want json to be used. bodyParser.urlencoded({extended: ...}) basically tells the system whether you want to use a simple algorithm for shallow parsing (i.e. false) or complex algorithm for deep parsing that can deal with nested objects (i.e. true)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+  console.log("app is listening on: " + PORT);
+});
